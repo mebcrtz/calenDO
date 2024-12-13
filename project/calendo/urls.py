088e76b492472ff7abce4a62f701caa34be1a623
mebcrtz <81579471+mebcrtz@users.noreply.github.com>
2024-12-13 23:28:59 +0800

    able to create tasks

--- project/calendo/urls.py
+++ project/calendo/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.index, name='calendo-index'),
     path('calendar/', views.calendar, name='calendo-calendar'),
     path('todo/', views.todo, name='calendo-todo'),
+    path('create-task/', views.create_task, name='create_task'),
 ]
--- project/calendo/views.py
+++ project/calendo/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.http import JsonResponse
 from django.http import HttpResponse
 
 from .forms import *
@@ -6,20 +7,28 @@ from .models import *
 
 # Create your views here.
 def index(request):
+    # Organize tasks by section
     tasks = Task.objects.all()
-    form = TaskForm()
+    sections_and_tasks = {}
+    for task in tasks:
+        if task.section not in sections_and_tasks:
+            sections_and_tasks[task.section] = []
+        sections_and_tasks[task.section].append(task)
+    return render(request, 'todo/index.html', {'sections_and_tasks': sections_and_tasks})
 
+def create_task(request):
     if request.method == 'POST':
         form = TaskForm(request.POST)
         if form.is_valid():
             form.save()
-        return redirect('/')
+            return redirect('calendo-index')
+        else:
+            return JsonResponse({'success': False, 'errors': form.errors})
+    else:
+        form = TaskForm()
+    return render(request, 'modal.html', {'form': form})
 
-    context = {
-        'tasks': tasks,
-        'form': form,
-    }
-    return render(request, 'todo/index.html', context)
+
 
 def calendar(request):
     return HttpResponse("This is the calendar page")
